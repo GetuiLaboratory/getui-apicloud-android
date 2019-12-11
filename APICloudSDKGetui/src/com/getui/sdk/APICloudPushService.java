2a3c8f5e53474e1f2d9049491ea504b291cba88a
xingk <xingk@getui.com>
2019-12-11 10:35:38 +0800

    更新sdk到4.3.5.10

--- APICloudSDKGetui/src/com/getui/sdk/APICloudPushService.java
+++ APICloudSDKGetui/src/com/getui/sdk/APICloudPushService.java
@@ -1,11 +1,6 @@
 package com.getui.sdk;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.util.Log;
-
-import com.igexin.sdk.GTServiceManager;
+import com.igexin.sdk.PushService;
 
 /**
  * 核心服务, 继承 android.app.Service, 必须实现以下几个接口, 并在 AndroidManifest 声明该服务并配置成
@@ -13,50 +8,50 @@ import com.igexin.sdk.GTServiceManager;
  * PushManager.getInstance().initialize(this.getApplicationContext(), userPushService), 其中
  * userPushService 为 用户自定义服务 即 DemoPushService.
  */
-public class APICloudPushService extends Service {
+public class APICloudPushService extends PushService {
 
     private static final String TAG = "APICloudPushService";
 
 
-    @Override
-    public void onCreate() {
-        Log.d(TAG, TAG + " call -> onCreate -------");
-
-        super.onCreate();
-        GTServiceManager.getInstance().onCreate(this);
-    }
-
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, TAG + " call -> onStartCommand -------");
-
-        super.onStartCommand(intent, flags, startId);
-        return GTServiceManager.getInstance().onStartCommand(this, intent, flags, startId);
-    }
-
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.d(TAG, "onBind -------");
-        return GTServiceManager.getInstance().onBind(intent);
-    }
-
-
-    @Override
-    public void onDestroy() {
-        Log.d(TAG, "onDestroy -------");
-
-        super.onDestroy();
-        GTServiceManager.getInstance().onDestroy();
-    }
-
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        GTServiceManager.getInstance().onLowMemory();
-    }
+//    @Override
+//    public void onCreate() {
+//        Log.d(TAG, TAG + " call -> onCreate -------");
+//
+//        super.onCreate();
+//        GTServiceManager.getInstance().onCreate(this);
+//    }
+//
+//
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//        Log.d(TAG, TAG + " call -> onStartCommand -------");
+//
+//        super.onStartCommand(intent, flags, startId);
+//        return GTServiceManager.getInstance().onStartCommand(this, intent, flags, startId);
+//    }
+//
+//
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        Log.d(TAG, "onBind -------");
+//        return GTServiceManager.getInstance().onBind(intent);
+//    }
+//
+//
+//    @Override
+//    public void onDestroy() {
+//        Log.d(TAG, "onDestroy -------");
+//
+//        super.onDestroy();
+//        GTServiceManager.getInstance().onDestroy();
+//    }
+//
+//
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        GTServiceManager.getInstance().onLowMemory();
+//    }
 
 
 }
